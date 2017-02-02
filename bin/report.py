7554651944cec6176259fad11936402363c66a45
Paul Downey <psd@whatfettle.com>
2017-02-02 13:54:18 +0000

    More legislation

--- bin/report.py
+++ bin/report.py
@@ -2,10 +2,33 @@
 
 import csv
 
+register_path = 'data/internal-drainage-board/internal-drainage-boards.tsv'
+legislation_path = 'lists/legislation/list.tsv'
+
 
 def legislation_url(legislation):
     slug = legislation.replace('-', '/')
     return "http://www.legislation.gov.uk/%s/made" % (slug)
+
+idbs = {}
+
+# find cited legislation ..
+legislations = {}
+
+def add_legislation(legislation, name='', idb=None):
+    if legislation not in legislations:
+        legislations[legislation] = { 'idbs': [] }
+    legislations[legislation]['name'] = name
+    if idb:
+        legislations[legislation]['idbs'].append(idb)
+
+
+for row in csv.DictReader(open(register_path), delimiter='\t'):
+    add_legislation(row['legislation'], '', row['internal-drainage-board'])
+    idbs[row['internal-drainage-board']] = row
+
+for row in csv.DictReader(open(legislation_path), delimiter='\t'):
+    add_legislation(row['legislation'], row['name'])
 
 
 print("""
@@ -51,13 +74,10 @@ td {
 <tbody>
 """)
 
-key = 'internal-drainage-board'
-
-register_path = 'data/internal-drainage-board/internal-drainage-boards.tsv'
 for row in csv.DictReader(open(register_path), delimiter='\t'):
 
-    print("<tr id='%s'>" % (row[key]))
-    print("<td>%s</td>" % (row[key]))
+    print("<tr id='%s'>" % (row['internal-drainage-board']))
+    print("<td>%s</td>" % (row['internal-drainage-board']))
     print("<td class='name'>%s</td>" % (row['name']))
     print("<td><a href='%s'>%s</a></td>" % (legislation_url(row['legislation']), row['legislation']))
     print("<td>%s</td>" % (row['start-date']))
@@ -75,21 +95,27 @@ print("""
 <thead>
     <tr>
       <th class='id'>legislation</th>
+      <th>IDBs</th>
       <th>name</th>
     </tr>
 </thead>
 <tbody>
 """)
 
-key = 'legislation'
+for legislation in sorted(legislations):
 
-path = 'lists/legislation/list.tsv'
-for row in csv.DictReader(open(path), delimiter='\t'):
+    if legislation:
+        row = legislations[legislation]
+        print("<tr id='%s'>" % (legislation))
 
-    print("<tr id='%s'>" % (row[key]))
-    print("<td><a href='%s'>%s</a></td>" % (legislation_url(row['legislation']), row['legislation']))
-    print("<td class='name'>%s</td>" % (row['name']))
-    print("</tr>")
+        idb_links = []
+        for idb in row['idbs']:
+            idb_links.append('<a href="#%s" title="%s">%s</a>' % (idb, idbs[idb]['name'], idb))
+
+        print("<td>" + ", ".join(idb_links) + "</td>")
+        print("<td><a href='%s'>%s</a></td>" % (legislation_url(legislation), legislation))
+        print("<td class='name'>%s</td>" % (row['name']))
+        print("</tr>")
 
 print("""
 </tbody>
